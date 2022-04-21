644057849044535c1b7fdc4e86354042df010a0d
tak6831 <tak6831@gmail.com>
2022-04-21 23:06:08 +0900

    fix: Fix method

--- src/main/java/com/codestatesinfoserviceserver/config/InfoRouter.java
+++ src/main/java/com/codestatesinfoserviceserver/config/InfoRouter.java
@@ -3,19 +3,20 @@ package com.codestatesinfoserviceserver.config;
 import com.codestatesinfoserviceserver.handler.WebfluxHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
-@Configuration
-@EnableWebFlux
-public class WebConfig implements WebFluxConfigurer {
+@Configuration(proxyBeanMethods = false)
+public class InfoRouter {
+
     @Bean
-    public RouterFunction<ServerResponse> routes(WebfluxHandler handler) {
-        return RouterFunctions.route(GET("/info"), handler::getInfo);
+    public RouterFunction<ServerResponse> route(){
+        return RouterFunctions.route(GET("/info")
+                .and(accept(MediaType.APPLICATION_JSON)), WebfluxHandler::getInfo);
     }
 }
--- src/main/java/com/codestatesinfoserviceserver/handler/WebfluxHandler.java
+++ src/main/java/com/codestatesinfoserviceserver/handler/WebfluxHandler.java
@@ -3,17 +3,19 @@ package com.codestatesinfoserviceserver.handler;
 import com.codestatesinfoserviceserver.entity.Infos;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-@Component
 public class WebfluxHandler {
-    public Mono<ServerResponse> getInfo(ServerRequest request) {
 
-        String name = request.queryParam("name").get();
-        Infos message = new Infos("", "testJob");
-        System.out.println("받았지롱");
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(message);
+    public static Mono<ServerResponse> getInfo(ServerRequest request) {
+        String name = request.queryParam("name").orElse("User");
+//        String name = request.queryParam("name").get(); 이거 안됌
+        Mono<Infos> infoData = Mono.just(new Infos(name,"testJob"));
+
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(infoData, Infos.class);
+//        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(infoData); 이것도 안됨.
     }
 }
